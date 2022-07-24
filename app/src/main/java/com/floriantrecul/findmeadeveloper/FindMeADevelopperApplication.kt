efe0d9108298b1489738603a31ab33ce280faf31
FlorianTrecul <trecul.florian@gmail.com>
2022-07-24 10:22:48 +0200

    Setup dagger hilt

---
+++ app/src/main/java/com/floriantrecul/findmeadeveloper/FindMeADevelopperApplication.kt
@@ -0,0 +1,7 @@
+package com.floriantrecul.findmeadeveloper
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FindMeADeveloperApplication: Application() {}
