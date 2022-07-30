d4a462d7779189428dc89c2b3a874cbb9f025d3a
FlorianTrecul <trecul.florian@gmail.com>
2022-07-30 14:54:05 +0200

    Add a Timber log library

--- app/src/main/java/com/floriantrecul/findmeadeveloper/FindMeADevelopperApplication.kt
+++ app/src/main/java/com/floriantrecul/findmeadeveloper/FindMeADevelopperApplication.kt
@@ -2,6 +2,19 @@ package com.floriantrecul.findmeadeveloper
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class FindMeADeveloperApplication: Application() {}
+class FindMeADeveloperApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        plantTimber()
+    }
+
+    private fun plantTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
